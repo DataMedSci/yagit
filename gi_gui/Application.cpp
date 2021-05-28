24e2fc28167c2be0eecba0543e11f1e1ee5e0491
Michał Kotula <micmich1999@gmail.com>
2021-05-28 16:16:56 +0200

    Added initial cmake for platform independent compilation (currently tested on windows only)

--- gi_gui/Application.cpp
+++ gi_gui/Application.cpp
@@ -15,6 +15,7 @@
  * along with 'yet Another Gamma Index Tool'; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *********************************************************************************************************************/
+
 #include "Application.h"
 #include "MainFrame.h"
 
