05aed78d1ab9d8871d0f79a08b4d438ee5a10f4d
kdbogusz <konrad_bogusz@wp.pl>
2021-06-16 11:59:48 +0200

    finished writer

--- gi_wrapper_cpp_gdcm/dicom_writer.cpp
+++ gi_wrapper_cpp_gdcm/dicom_writer.cpp
@@ -43,6 +43,40 @@ DicomWriter::~DicomWriter()
 {
 }
 
+void DicomWriter::saveImage(string filepath, File& oldFile, double* gamma,
+    int xNumber, int yNumber, int zNumber, int precision, double fillValue,
+    int rewriteTagsStrategyId, set<pair<int, int> > tags, int slice)
+{
+    ImageReader oldImageReader;
+    oldImageReader.SetFile(oldFile);
+
+    Image oldImage = oldImageReader.GetImage();
+    DataSet& oldDataSet = oldFile.GetDataSet();
+    PixelFormat pixelFormat = oldImage.GetPixelFormat();
+    PhotometricInterpretation photometricInterpretation = oldImage.GetPhotometricInterpretation();
+    int rescale = pow(10, precision);
+
+    Writer newWriter;
+    newWriter.SetFileName(filepath.c_str());
+    File& newFile = newWriter.GetFile();
+    DataSet& newDataSet = newFile.GetDataSet();
+    newFile.GetHeader().SetDataSetTransferSyntax(TransferSyntax().ExplicitVRLittleEndian);
+
+    insertFrames(newFile, pixelFormat, photometricInterpretation, gamma, xNumber, yNumber, zNumber, rescale, fillValue, slice);
+
+    // Copy seleceted dicom tags.
+    applyDicomTagsRewriteStrategy(oldDataSet, newDataSet, rewriteTagsStrategyId, tags);
+
+    // Set tags allowing to rescale value stored in DICOM pixel array into expected units.
+    setDicomRescaleTags(newDataSet, rescale);
+
+    if (!newWriter.Write())
+    {
+        cerr << "Could not write: " << filepath << endl;
+        exit(-1);
+    }
+}
+
 void DicomWriter::setDicomRescaleTags(DataSet& newDataSet, int rescale)
 {
     if (newDataSet.FindDataElement(Tag(0x0028, 0x1053)))
@@ -133,3 +167,309 @@ void DicomWriter2D::insertFrames(File& newFile, PixelFormat pf, PhotometricInter
     writer.SetImage(image);
 }
 
+
+DicomWriter3D::DicomWriter3D()
+    : DicomWriter()
+{
+}
+
+DicomWriter3D::~DicomWriter3D()
+{
+}
+
+void DicomWriter3D::insertFrames(File& newFile, PixelFormat pf, PhotometricInterpretation pi,
+    double* gamma, int xNumber, int yNumber, int zNumber, int rescale, double fillValue, int)
+{
+    DataSet& newDataSet = newFile.GetDataSet();
+
+    Image image = Image();
+    image.SetNumberOfDimensions(2);
+    image.SetDimension(0, xNumber);
+    image.SetDimension(1, yNumber);
+    image.SetPixelFormat(pf);
+    image.SetPhotometricInterpretation(pi);
+
+
+    {
+        int pixelSize = (int)image.GetPixelFormat().GetPixelSize();
+        char* buffer = new char[xNumber * yNumber * zNumber * pixelSize];
+        for (int k = 0; k < zNumber; k++) 
+        {
+            for (int j = 0; j < yNumber; j++)
+            {
+                for (int i = 0; i < xNumber; i++)
+                {
+                    int toFill;
+                    if (!std::isnan(gamma[(k * yNumber + j) * xNumber + i]))
+                        toFill = (int)(gamma[(k * yNumber + j) * xNumber + i] * rescale);
+                    else
+                        toFill = (int)(fillValue * rescale);
+
+                    if (pixelSize == 1) {
+                        _int8 currentValue = (_int8)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 2) {
+                        _int16 currentValue = (_int16)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 4) {
+                        _int32 currentValue = (_int32)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 8) {
+                        _int64 currentValue = (_int64)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else {
+                        cerr << "Error. Wrong pixel size " << pixelSize << "." << endl;
+                        exit(-1);
+                    }
+                }
+            }
+        }
+        DataElement pixeldata(Tag(0x7fe0, 0x0010));
+        pixeldata.SetByteValue(buffer, xNumber * yNumber * zNumber * pixelSize);
+        delete[] buffer;
+        image.SetDataElement(pixeldata);
+    }
+
+    ImageWriter writer;
+    writer.SetFile(newFile);
+    writer.SetImage(image);
+}
+
+
+DicomWriter3DSliceXY::DicomWriter3DSliceXY()
+    : DicomWriter()
+{
+}
+
+DicomWriter3DSliceXY::~DicomWriter3DSliceXY()
+{
+}
+
+void DicomWriter3DSliceXY::insertFrames(File& newFile, PixelFormat pf, PhotometricInterpretation pi,
+    double* gamma, int xNumber, int yNumber, int zNumber,
+    int rescale, double fillValue, int zSlice)
+{
+    if (zSlice < 0 || zSlice >= zNumber)
+    {
+        cerr << "Error. zSlice outside the image." << endl;
+        exit(-1);
+    }
+
+    DataSet& newDataSet = newFile.GetDataSet();
+
+    Image image = Image();
+    image.SetNumberOfDimensions(2);
+    image.SetDimension(0, xNumber);
+    image.SetDimension(1, yNumber);
+    image.SetPixelFormat(pf);
+    image.SetPhotometricInterpretation(pi);
+
+    {
+        int pixelSize = (int)image.GetPixelFormat().GetPixelSize();
+        char* buffer = new char[xNumber * yNumber * zNumber * pixelSize];
+        for (int k = 0; k < zNumber; k++)
+        {
+            for (int j = 0; j < yNumber; j++)
+            {
+                for (int i = 0; i < xNumber; i++)
+                {
+                    int toFill;
+                    if (k == zSlice && !std::isnan(gamma[j * xNumber + i]))
+                        toFill = (int)(gamma[j * xNumber + i] * rescale);
+                    else
+                        toFill = (int)(fillValue * rescale);
+
+                    if (pixelSize == 1) {
+                        _int8 currentValue = (_int8)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 2) {
+                        _int16 currentValue = (_int16)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 4) {
+                        _int32 currentValue = (_int32)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 8) {
+                        _int64 currentValue = (_int64)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else {
+                        cerr << "Error. Wrong pixel size " << pixelSize << "." << endl;
+                        exit(-1);
+                    }
+                }
+            }
+        }
+        DataElement pixeldata(Tag(0x7fe0, 0x0010));
+        pixeldata.SetByteValue(buffer, xNumber * yNumber * zNumber * pixelSize);
+        delete[] buffer;
+        // not sure if I understand the purpose of it well enough, might need a change
+        image.SetDataElement(pixeldata);
+    }
+
+    ImageWriter writer;
+    writer.SetFile(newFile);
+    writer.SetImage(image);
+}
+
+
+DicomWriter3DSliceXZ::DicomWriter3DSliceXZ()
+    : DicomWriter()
+{
+}
+
+DicomWriter3DSliceXZ::~DicomWriter3DSliceXZ()
+{
+}
+
+void DicomWriter3DSliceXZ::insertFrames(File& newFile, PixelFormat pf, PhotometricInterpretation pi,
+    double* gamma, int xNumber, int yNumber, int zNumber,
+    int rescale, double fillValue, int ySlice)
+{
+    if (ySlice < 0 || ySlice >= yNumber)
+    {
+        cerr << "Error. ySlice outside the image." << endl;
+        exit(-1);
+    }
+
+    DataSet& newDataSet = newFile.GetDataSet();
+
+    Image image = Image();
+    image.SetNumberOfDimensions(2);
+    image.SetDimension(0, xNumber);
+    image.SetDimension(1, yNumber);
+    image.SetPixelFormat(pf);
+    image.SetPhotometricInterpretation(pi);
+
+    {
+        int pixelSize = (int)image.GetPixelFormat().GetPixelSize();
+        char* buffer = new char[xNumber * yNumber * zNumber * pixelSize];
+        for (int k = 0; k < zNumber; k++)
+        {
+            for (int j = 0; j < yNumber; j++)
+            {
+                for (int i = 0; i < xNumber; i++)
+                {
+                    int toFill;
+                    if (j == ySlice && !std::isnan(gamma[k * xNumber + i]))
+                        toFill = (int)(gamma[k * xNumber + i] * rescale);
+                    else
+                        toFill = (int)(fillValue * rescale);
+
+                    if (pixelSize == 1) {
+                        _int8 currentValue = (_int8)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 2) {
+                        _int16 currentValue = (_int16)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 4) {
+                        _int32 currentValue = (_int32)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 8) {
+                        _int64 currentValue = (_int64)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else {
+                        cerr << "Error. Wrong pixel size " << pixelSize << "." << endl;
+                        exit(-1);
+                    }
+                }
+            }
+        }
+        DataElement pixeldata(Tag(0x7fe0, 0x0010));
+        pixeldata.SetByteValue(buffer, xNumber * yNumber * zNumber * pixelSize);
+        delete[] buffer;
+        image.SetDataElement(pixeldata);
+    }
+
+    ImageWriter writer;
+    writer.SetFile(newFile);
+    writer.SetImage(image);
+}
+
+
+DicomWriter3DSliceYZ::DicomWriter3DSliceYZ()
+    : DicomWriter()
+{
+}
+
+DicomWriter3DSliceYZ::~DicomWriter3DSliceYZ()
+{
+}
+
+void DicomWriter3DSliceYZ::insertFrames(File& newFile, PixelFormat pf, PhotometricInterpretation pi,
+    double* gamma, int xNumber, int yNumber, int zNumber,
+    int rescale, double fillValue, int xSlice)
+{
+    if (xSlice < 0 || xSlice >= xNumber)
+    {
+        cerr << "Error. xSlice outside the image." << endl;
+        exit(-1);
+    }
+
+    DataSet& newDataSet = newFile.GetDataSet();
+
+    Image image = Image();
+    image.SetNumberOfDimensions(2);
+    image.SetDimension(0, xNumber);
+    image.SetDimension(1, yNumber);
+    image.SetPixelFormat(pf);
+    image.SetPhotometricInterpretation(pi);
+
+    {
+        int pixelSize = (int)image.GetPixelFormat().GetPixelSize();
+        char* buffer = new char[xNumber * yNumber * zNumber * pixelSize];
+        for (int k = 0; k < zNumber; k++)
+        {
+            for (int j = 0; j < yNumber; j++)
+            {
+                for (int i = 0; i < xNumber; i++)
+                {
+                    int toFill;
+                    if (i == xSlice && !std::isnan(gamma[k * yNumber + i]))
+                        toFill = (int)(gamma[k * yNumber + i] * rescale);
+                    else
+                        toFill = (int)(fillValue * rescale);
+
+                    if (pixelSize == 1) {
+                        _int8 currentValue = (_int8)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 2) {
+                        _int16 currentValue = (_int16)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 4) {
+                        _int32 currentValue = (_int32)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else if (pixelSize == 8) {
+                        _int64 currentValue = (_int64)toFill;
+                        memcpy(&buffer[((k * yNumber + j) * xNumber + i) * pixelSize], &currentValue, pixelSize);
+                    }
+                    else {
+                        cerr << "Error. Wrong pixel size " << pixelSize << "." << endl;
+                        exit(-1);
+                    }
+                }
+            }
+        }
+        DataElement pixeldata(Tag(0x7fe0, 0x0010));
+        pixeldata.SetByteValue(buffer, xNumber * yNumber * zNumber * pixelSize);
+        delete[] buffer;
+        image.SetDataElement(pixeldata);
+    }
+
+    ImageWriter writer;
+    writer.SetFile(newFile);
+    writer.SetImage(image);
+}
--- gi_wrapper_cpp_gdcm/dicom_writer.h
+++ gi_wrapper_cpp_gdcm/dicom_writer.h
@@ -1,3 +1,5 @@
+/** @file */
+
 /*********************************************************************************************************************
  * This file is part of 'yet Another Gamma Index Tool'.
  *
@@ -36,6 +38,64 @@ public:
 
     virtual ~DicomWriter();
 
+/// \brief Saves Gamma Index array and all necessary tags to a DICOM file.
+///
+/// \note Values of parameters: dims, refXNumber, refYNumber, refZNumber, plane, refSlice
+///       should be the same as those passed to calculateGamma() function.
+///
+/// \param filepath              Path where the output DICOM file will be saved.
+/// \param oldFile               Reference to the object in which GDCM library stored parsed reference DICOM file.
+///                              The reference returned by loadDicom() function. Used while transferring tags to the output DICOM file.
+/// \param gamma                 The calculated gamma array.
+/// \param xNumber               Number of voxels along x axis in the gamma image.
+/// \param yNumber               Number of voxels along y axis in the gamma image.
+/// \param zNumber               Number of voxels along z axis in the gamma image.
+/// \param precision             Number of digits after the decimal point in the resulting Gamma Index.
+/// \param fillValue             Value that will be assigned to voxels for which the GI comparison was not performed.
+/// \param rewriteTagsStrategyId Specifies the strategy of transferring tags from reference DICOM to output DICOM file:
+///                              - 5 - rewrite all tags, except these specified in tags parameter
+///                              - 4 - rewrite only these specified in tags parameter
+///                              - 3 - rewrite only not private tags
+///                              - 2 - rewrite no tags
+///                              - 1 or other integer values - rewrite all tags
+/// \param tags                  (optional) Required only rewriteTagsStrategy dims is greater than 3.
+///                              The set specifying which exactly tags (not) to transfer.
+///                              For more information on how to construct this set see applyDicomTagsRewriteStrategy().
+/// \param slice                 (optional) Required only when GI comparison was performed in 2.5D od 2dfrom3D version.
+///                              Specifies for which slice (in the given orientation) the GI comparison was performed.
+///                              First slice has number 0.
+///
+///////////////////////////////////////////////////////////////////////////////
+    void saveImage(std::string filepath, gdcm::File& oldFile, double* gamma,
+        int xNumber, int yNumber, int zNumber, int precision, double fillValue,
+        int rewriteTagsStrategyId, std::set<std::pair<int, int> > tags = (std::set<std::pair<int, int> >()),
+        int slice = -1);
+
+protected:
+
+    /// \brief Copies Gamma Index array to a DICOM matrix.
+    ///
+    /// \note Values of parameters: xNumber, yNumber, zNumber, plane, slice
+    ///       should be the same as those passed to calculateGamma() function.
+    ///
+    /// \param newFile    Object in which the new DICOM is constructed.
+    /// \param pf         The pixel format of DICOM image (GDCM parameter)
+    /// \param pi         The photometric interpretation of DICOM image (GDCM parameter)
+    /// \param gamma      The calculated gamma array.
+    /// \param xNumber    Number of voxels along x axis in the gamma image.
+    /// \param yNumber    Number of voxels along y axis in the gamma image.
+    /// \param zNumber    Number of voxels along z axis in the gamma image.
+    /// \param rescale    Value by which the GI values are rescaled before inserting into DICOM,
+    ///                   in order to preserve the precision.
+    /// \param fillValue  Value that will be assigned to voxels for which the GI comparison was not performed.
+    /// \param slice      (optional) Required only when GI comparison was performed in 2.5D od 2dfrom3D version.
+    ///                   Specifies for which slice (in the given orientation) the GI comparison was performed.
+    ///                   First slice has number 0.
+    ///
+    ///////////////////////////////////////////////////////////////////////////////
+    virtual void insertFrames(gdcm::File& newFile, gdcm::PixelFormat pf, gdcm::PhotometricInterpretation pi,
+        double* gamma, int xNumber, int yNumber, int zNumber, int rescale, double fillValue, int slice) = 0;
+
 private:
     void setDicomRescaleTags(gdcm::DataSet& newDataSet, int rescale);
 };
@@ -51,6 +111,54 @@ public:
 protected:
     virtual void insertFrames(gdcm::File& newFile, gdcm::PixelFormat pf, gdcm::PhotometricInterpretation pi,
         double* gamma, int xNumber, int yNumber, int, int rescale, double fillValue, int);
+};
+
+class DicomWriter3D : public DicomWriter
+{
+public:
+    DicomWriter3D();
+
+    virtual ~DicomWriter3D();
+
+protected:
+    virtual void insertFrames(gdcm::File& newFile, gdcm::PixelFormat pf, gdcm::PhotometricInterpretation pi,
+        double* gamma, int xNumber, int yNumber, int zNumber, int rescale, double fillValue, int);
+};
+
+class DicomWriter3DSliceXY : public DicomWriter
+{
+public:
+    DicomWriter3DSliceXY();
+
+    virtual ~DicomWriter3DSliceXY();
+
+protected:
+    virtual void insertFrames(gdcm::File& newFile, gdcm::PixelFormat pf, gdcm::PhotometricInterpretation pi,
+        double* gamma, int xNumber, int yNumber, int zNumber, int rescale, double fillValue, int zSlice);
+};
+
+class DicomWriter3DSliceXZ : public DicomWriter
+{
+public:
+    DicomWriter3DSliceXZ();
+
+    virtual ~DicomWriter3DSliceXZ();
+
+protected:
+    virtual void insertFrames(gdcm::File& newFile, gdcm::PixelFormat pf, gdcm::PhotometricInterpretation pi,
+        double* gamma, int xNumber, int yNumber, int zNumber, int rescale, double fillValue, int ySlice);
+};
+
+class DicomWriter3DSliceYZ : public DicomWriter
+{
+public:
+    DicomWriter3DSliceYZ();
+
+    virtual ~DicomWriter3DSliceYZ();
+
+protected:
+    virtual void insertFrames(gdcm::File& newFile, gdcm::PixelFormat pf, gdcm::PhotometricInterpretation pi,
+        double* gamma, int xNumber, int yNumber, int zNumber, int rescale, double fillValue, int xSlice);
 };
 
 #endif
