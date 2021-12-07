9a146ae2db08cfb6f34110948a712a9b4eae06e2
Michał Kotula <ext-michal.kotula@varjo.com>
2021-12-07 11:54:43 +0100

    [core] Final polish + group include

--- gi_core/include/common.hpp
+++ gi_core/include/common.hpp
@@ -4,7 +4,12 @@
 
 #include <system_error>
 #include <memory>
+#include <array>
 #include <optional>
+#include <algorithm>
+#include <functional>
+#include <variant>
+#include <numeric>
 
 #if(__cplusplus <= 202002L)
 #define ALLOCATE_AT_LEAST_NOT_DEFINED
@@ -14,16 +19,43 @@ namespace yagit::core
 {
 	using size_t = std::size_t;
 	using std::error_code;
+	using std::unique_ptr;
 	using std::shared_ptr;
 	using std::enable_if_t;
 	using std::optional;
 	using std::nullopt;
+	using std::array;
 	using std::true_type;
 	using std::false_type;
 	using std::declval;
+	using std::index_sequence;
+	using std::make_index_sequence;
+	using std::variant;
+	using std::numeric_limits;
 #ifndef ALLOCATE_AT_LEAST_NOT_DEFINED
 	using std::allocation_result;
 #endif
+
+	template<typename T>
+	using view = T*;
+
+	template<typename T>
+	using const_view = const T*;
+
+	template<size_t N>
+	constexpr bool is_power_of_two_v = N && !(N & (N - 1));
+
+	template<size_t ByteCount, typename ElementType, typename = std::enable_if_t<is_power_of_two_v<ByteCount>>>
+	bool is_aligned_to_bytes(const_view<ElementType> ptr)
+	{
+		return (reinterpret_cast<std::uintptr_t>(ptr) & ByteCount == 0);
+	}
+
+	template<size_t ElementCount, typename ElementType, typename = std::enable_if_t<is_power_of_two_v<ElementCount>>>
+	bool is_aligned_to(const_view<ElementType> ptr)
+	{
+		return is_aligned_to_bytes<sizeof(ElementType)* ElementCount>(ptr);
+	}
 
 	namespace data
 	{
@@ -35,7 +67,6 @@ namespace yagit::core
 			size_t count;
 		};
 #endif
-
 
 		namespace detail
 		{
@@ -72,10 +103,22 @@ namespace yagit::core
 		/// </summary>
 		/// <typeparam name="Dimensions">Dimensionality of the index offset</typeparam>
 		template<size_t Dimensions>
-		struct offset
+		struct offsets
 		{
-			size_t offsets[Dimensions];
+			size_t offset[Dimensions];
 		};
+
+		template<size_t Dimensions>
+		constexpr bool operator==(const offsets<Dimensions>& left, const offsets<Dimensions>& right) noexcept
+		{
+			return std::equal(std::begin(left.offset), std::end(left.offset), std::begin(right.offset), std::end(right.offset), std::equal_to<size_t>());
+		}
+
+		template<size_t Dimensions>
+		constexpr bool operator!=(const offsets<Dimensions>& left, const offsets<Dimensions>& right) noexcept
+		{
+			return !(left == right);
+		}
 
 		/// <summary>
 		/// <paramref name="Dimensions"/>-dimensional size
@@ -88,9 +131,21 @@ namespace yagit::core
 		};
 
 		template<size_t Dimensions>
+		constexpr bool operator==(const sizes<Dimensions>& left, const sizes<Dimensions>& right) noexcept
+		{
+			return std::equal(std::begin(left.sizes), std::end(left.sizes), std::begin(right.sizes), std::end(right.sizes), std::equal_to<size_t>());
+		}
+
+		template<size_t Dimensions>
+		constexpr bool operator!=(const sizes<Dimensions>& left, const sizes<Dimensions>& right) noexcept
+		{
+			return !(left == right);
+		}
+
+		template<size_t Dimensions>
 		constexpr size_t total_size(const sizes<Dimensions>& size)
 		{
-			return std::accumulate(dimension_sizes.begin(), dimension_sizes.end(), static_cast<size_t>(0), std::multiplies<size_t>());
+			return std::accumulate(std::begin(size.sizes), std::end(size.sizes), static_cast<size_t>(0), std::multiplies<size_t>());
 		}
 
 		/// <summary>
@@ -101,17 +156,39 @@ namespace yagit::core
 		template<size_t Dimensions>
 		struct data_region
 		{
-			offset<Dimensions> offset;
+			offsets<Dimensions> offset;
 			sizes<Dimensions> size;
 		};
 
-		struct order_element_t { size_t dimension; };
+		template<size_t Dimensions>
+		constexpr bool operator==(const data_region<Dimensions>& left, const data_region<Dimensions>& right) noexcept
+		{
+			return left.offset == right.offset && left.size == right.size;
+		}
+
+		template<size_t Dimensions>
+		constexpr bool operator!=(const data_region<Dimensions>& left, const data_region<Dimensions>& right) noexcept
+		{
+			return left.offset != right.offset || left.size != right.size;
+		}
+
+		struct order_element_t { size_t dimension_index; };
 
 		constexpr order_element_t dim_x { 0 };
 		constexpr order_element_t dim_y { 1 };
 		constexpr order_element_t dim_z { 2 };
 		template<size_t Dimension>
 		constexpr order_element_t dim = { Dimension };
+
+		constexpr bool operator==(const order_element_t& left, const order_element_t& right) noexcept
+		{
+			return left.dimension_index == right.dimension_index;
+		}
+
+		constexpr bool operator!=(const order_element_t& left, const order_element_t& right) noexcept
+		{
+			return left.dimension_index != right.dimension_index;
+		}
 
 		/// <summary>
 		/// Structure describing stored data format
@@ -120,12 +197,33 @@ namespace yagit::core
 		template<size_t Dimensions>
 		struct data_format
 		{
-			const std::array<order_element_t, Dimensions> order;
+			const array<order_element_t, Dimensions> order;
 		};
 
+		namespace detail
+		{
+			template<size_t Dimensions, size_t... I>
+			constexpr array<order_element_t, Dimensions> make_default_order(index_sequence<I...>)
+			{
+				return { dim<I>... };
+			}
+		}
+
 		template<size_t Dimensions>
-		constexpr data_format<Dimensions> cpp_multidimensional_array_format{ dim_x, dim_y, dim_z };
+		constexpr data_format<Dimensions> cpp_multidimensional_array_format{ detail::make_default_order<Dimensions>(make_index_sequence<Dimensions>()) };
 		template<size_t Dimensions>
-		constexpr data_format<Dimensions> default_format = cpp_multidimensional_array_format;
+		constexpr data_format<Dimensions> default_format = cpp_multidimensional_array_format<Dimensions>;
+
+		template<size_t Dimensions>
+		constexpr bool operator==(const data_format<Dimensions>& left, const data_format<Dimensions>& right) noexcept
+		{
+            return std::equal(std::begin(left.order), std::end(left.order), std::begin(right.order), std::end(right.order), std::equal_to<order_element_t>());
+		}
+
+		template<size_t Dimensions>
+		constexpr bool operator!=(const data_format<Dimensions>& left, const data_format<Dimensions>& right) noexcept
+		{
+			return !(left == right);
+		}
 	}
 }
--- gi_core/include/definitions.hpp
+++ gi_core/include/definitions.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 namespace yagit::core
 {
 	using size_t = std::size_t;
+	using ptrdiff_t = std::ptrdiff_t;
 
 	namespace data
 	{
 		template<size_t Dimensions>
-		struct offset;
+		struct offsets;
 		
 		template<size_t Dimensions>
 		struct sizes;
