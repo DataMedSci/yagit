4990609853be4e24e377b0a892ec541c9e2ebff3
michal367 <michal.krawczyk7@gmail.com>
2023-05-19 16:16:09 +0200

    change license message in source files

--- new/examples/gamma25D.cpp
+++ new/examples/gamma25D.cpp
@@ -1,9 +1,11 @@
-/******************************************************************************************
+/********************************************************************************************
+ * Copyright (C) 2023 'yet Another Gamma Index Tool' Developers.
+ * 
  * This file is part of 'yet Another Gamma Index Tool'.
- *
- * 'yet Another Gamma Index Tool' is free software; you can redistribute it and/or modify
+ * 
+ * 'yet Another Gamma Index Tool' is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * 'yet Another Gamma Index Tool' is distributed in the hope that it will be useful,
@@ -12,9 +14,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with 'yet Another Gamma Index Tool'; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- ******************************************************************************************/
+ * along with 'yet Another Gamma Index Tool'.  If not, see <http://www.gnu.org/licenses/>.
+ ********************************************************************************************/
 /**
  * @file
  * @brief This file provides a simple example of using yagit - 2.5D gamma index.
