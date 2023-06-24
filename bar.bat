d2125d2213d28f0552af770905f2dc6e45c46f72
michal367 <michal.krawczyk7@gmail.com>
2023-06-25 00:36:39 +0200

    add building shared library

--- bar.bat
+++ bar.bat
@@ -1,23 +1,46 @@
 @echo off
 
-@REM echo CONFIGURING CMAKE FIRST TIME...
-@REM mkdir build
-@REM cd build
-@REM conan install ..
-@REM cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
-@REM cd ..
+set BUILD_TYPE=Release
+set BUILD_SHARED_LIBS=OFF
 
+@REM set GAMMA_VERSION=SEQUENTIAL
+set GAMMA_VERSION=THREADED
+
+set BUILD_EXAMPLES=ON
+set BUILD_PERFORMANCE_TESTING=OFF
+
+set REF_IMG=original_dose_beam_4.dcm
+set EVAL_IMG=logfile_dose_beam_4.dcm
+
+set INSTALL_DIR=./yagit
+
+
+@REM ============================================================
+if not exist build\CMakeCache.txt (
+    echo CONFIGURING CMAKE FIRST TIME...
+    mkdir build
+    cd build
+    conan install ..
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
+    cd ..
+)
+
+
+@REM ============================================================
 echo:
 echo CONFIGURING CMAKE...
 cd build
-@REM set GAMMA_VERSION=SEQUENTIAL
-set GAMMA_VERSION=THREADED
-set OTHER_OPTIONS=-DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DBUILD_PERFORMANCE_TESTING=ON
-cmake .. -DGAMMA_VERSION=%GAMMA_VERSION% %OTHER_OPTIONS%
+cmake .. -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ^
+         -DBUILD_SHARED_LIBS=%BUILD_SHARED_LIBS% ^
+         -DGAMMA_VERSION=%GAMMA_VERSION% ^
+         -DBUILD_EXAMPLES=%BUILD_EXAMPLES% ^
+         -DBUILD_PERFORMANCE_TESTING=%BUILD_PERFORMANCE_TESTING%
 
+
+@REM ============================================================
 echo:
 echo COMPILING...
-cmake --build . --config Release
+cmake --build . --config %BUILD_TYPE%
 set COMPILE_RESULT=%ERRORLEVEL%
 cd ..
 
@@ -25,19 +48,35 @@ if %COMPILE_RESULT% NEQ 0 (
     exit /b %COMPILE_RESULT%
 )
 
+
+@REM ============================================================
+if %BUILD_EXAMPLES% == ON (
+    echo:
+    echo RUNNING EXAMPLES...
+    build\examples\%BUILD_TYPE%\gamma2DInterp.exe %REF_IMG% %EVAL_IMG%
+    echo:
+    build\examples\%BUILD_TYPE%\gamma25D.exe %REF_IMG% %EVAL_IMG%
+    echo:
+    build\examples\%BUILD_TYPE%\gamma3D.exe %REF_IMG% %EVAL_IMG%
+    echo:
+    build\examples\%BUILD_TYPE%\gammaImage.exe
+)
+
+if %BUILD_PERFORMANCE_TESTING% == ON (
+    echo:
+    echo RUNNING PERFORMANCE TEST...
+    build\tests\performance\%BUILD_TYPE%\perfTest.exe %REF_IMG% %EVAL_IMG% gammaTimes.csv
+)
+
+
+@REM ============================================================
 echo:
-echo RUNNING...
-set REF_IMG=original_dose_beam_4.dcm
-set EVAL_IMG=logfile_dose_beam_4.dcm
-
-build\tests\performance\Release\perfTest.exe %REF_IMG% %EVAL_IMG% gammaTimes.csv
-
-@REM echo:
-@REM echo RUNNING EXAMPLES...
-@REM build\examples\Release\gamma2DInterp.exe %REF_IMG% %EVAL_IMG%
-@REM echo:
-@REM build\examples\Release\gamma25D.exe %REF_IMG% %EVAL_IMG%
-@REM echo:
-@REM build\examples\Release\gamma3D.exe %REF_IMG% %EVAL_IMG%
-@REM echo:
-@REM build\examples\Release\gammaImage.exe
+echo INSTALLING...
+IF "%INSTALL_DIR%" NEQ "" (
+    echo INSTALLING TO %INSTALL_DIR%
+    cmake --install build --prefix %INSTALL_DIR%
+) else (
+    echo INSTALLING TO SYSTEM DIRECTORY
+    echo MAKE SURE YOU RUN THIS AS ADMINISTRATOR
+    cmake --install build
+)
---
+++ cmake/Common.cmake
@@ -0,0 +1,14 @@
+function(copy_target_output source_target dest_target)
+    add_custom_command(
+        TARGET ${dest_target} POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            $<TARGET_FILE:${source_target}>
+            $<TARGET_FILE_DIR:${dest_target}>
+    )
+endfunction()
+
+function(copy_dll_to_exec dll_target exec_target)
+    if(WIN32 AND BUILD_SHARED_LIBS)
+        copy_target_output(${dll_target} ${exec_target})
+    endif()
+endfunction()
